149d9a3c5005d5c77e2ce655d0857908484641e3
YaFeng-Xiong <yafeng.xiong110@gmail.com>
2018-02-26 15:43:50 -0700

    More Cats Spawn!

---
+++ FBLA-Game/Assets/CatSpawn.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatSpawn : MonoBehaviour {
+
+    public GameObject[] cats;
+    public GameObject cat;
+
+
+	// Use this for initialization
+	void Start () {
+        cats = GameObject.FindGameObjectsWithTag("Respawn");
+        cat = GameObject.FindGameObjectWithTag("Respawn");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        cats = GameObject.FindGameObjectsWithTag("Respawn");
+        if (cats.Length < 3)
+        {
+            spawnCat();
+        }
+        else if(cats.Length > 3)
+        {
+            killCat();
+        }
+	}
+    void spawnCat()
+    {
+        Instantiate(cat, cats[0].transform.position, cats[0].transform.rotation);
+    }
+    void killCat()
+    {
+        Destroy(cats[0]);
+    }
+
+}
---
+++ FBLA-Game/Assets/CatSpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatSpawner : MonoBehaviour {
+
+    public GameObject[] cats;
+    public GameObject cat;
+
+	// Use this for initialization
+	void Start () {
+        cat = GameObject.FindGameObjectWithTag("Respawn");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        cats = GameObject.FindGameObjectsWithTag("Respawn");
+        if (cats.Length < 15)
+        {
+            spawnCat();
+        }
+        else if (cats.Length > 15)
+        {
+            //kill a cat
+        }
+    }
+    void spawnCat()
+    {
+        //find Random x and y coordinates within a certain radius
+        //Random xcoord = new Random(-10, 10);
+
+        Instantiate(cat, cats[1].transform.position, cats[1].transform.rotation);
+    }
+}
